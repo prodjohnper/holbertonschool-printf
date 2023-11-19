426b88d9a5f4b78484147d92393f5079b0c0bf78
prodjohnper <jonathanperez9743@gmail.com>
2023-11-19 15:02:52 -0400

    Update
    Refactored _printf function

--- _printf.c
+++ _printf.c
@@ -9,13 +9,13 @@
 int _printf(const char *format, ...)
 {
 	int count = 0, space = 0;
-	int i;
+	int i = 0;
 	va_list args;
+
 	va_start(args, format);
 
 	if (format == NULL)
 		return (-1);
-
 	for (i = 0; format[i]; i++)
 	{
 		if (format[i] == '%')
@@ -29,9 +29,9 @@ int _printf(const char *format, ...)
 			switch (format[i])
 			{
 				case 'c': case 's': case 'i': case 'd':
-					count += (format[i] == 'c') ? char_prnt(va_arg(args, int)) : 
+					count += (format[i] == 'c') ? char_prnt(va_arg(args, int)) :
 						(format[i] == 's') ? str_prnt(va_arg(args, char *)) :
-						int_prnt(va_arg(args, int)); 
+						int_prnt(va_arg(args, int));
 					break;
 				case '%':
 					write(1, &format[i], 1);
