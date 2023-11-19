804cd9d04d6a84b289cdaba19bc056c593704ff6
prodjohnper <45082645+prodjohnper@users.noreply.github.com>
2023-11-19 17:43:29 -0400

    Test
    Refactored def_prnt.c

--- def_prnt.c
+++ def_prnt.c
@@ -1,18 +1,12 @@
 #include "main.h"
 
 /**
- * def_prnt - Function that prints
- * '%' and a pointer to char.
+ * def_prnt - Function that prints '%'.
  *
- * @c: Char to print.
- *
- * Return: Number of char printed.
+ * Return: Nothing.
  */
 
-int def_prnt(char c)
+void def_prnt()
 {
 	write(1, "%", 1);
-	write(1, &c, 1);
-
-	return(2);
 }
