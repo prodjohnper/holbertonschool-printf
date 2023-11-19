f18aba4e5f416f720a250c7ac9f775bfb03c4060
prodjohnper <jonathanperez9743@gmail.com>
2023-11-19 10:40:41 -0400

    Update
    _printf function refactored

--- _printf.c
+++ _printf.c
@@ -1,71 +1,54 @@
 #include "main.h"
-
 /**
  * _printf - Function that produces
  * output according to a format.
- *
  * @format: Character string.
  *
  * Return: number of characters printed (excluding null byte).
  */
-
 int _printf(const char *format, ...)
 {
 	int count = 0, space = 0;
-	int i = 0;
-
+	int i;
 	va_list args;
-
 	va_start(args, format);
 
-	if (format != NULL)
+	if (format == NULL)
+		return (-1);
+
+	for (i = 0; format[i]; i++)
 	{
-		while (format[i])
+		if (format[i] == '%')
 		{
-			if (format[i] == '%')
+			i++;
+			while (format[i] == ' ')
 			{
 				i++;
-				while (format[i] == ' ')
-				{
-					i++;
-					space++;
-				}
-
-				switch (format[i])
-				{
-					case 'c':
-						count += char_prnt(va_arg(args, int));
-						break;
-					case 's':
-						count += str_prnt(va_arg(args, char *));
-						break;
-					case 'i':
-						count += int_prnt(va_arg(args, int));
-						break;
-					case 'd':
-						count += int_prnt(va_arg(args, int));
-						break;
-					case '%':
-						write(1, &format[i], 1);
-						count++;
-						break;
-					default:
-						count += def_prnt(format[i]);
-						i++;
-						break;
-				}
+				space++;
 			}
-			else
+			switch (format[i])
 			{
-				write(1, &format[i], 1);
-				count++;
+				case 'c': case 's': case 'i': case 'd':
+					count += (format[i] == 'c') ? char_prnt(va_arg(args, int)) : 
+						(format[i] == 's') ? str_prnt(va_arg(args, char *)) :
+						int_prnt(va_arg(args, int)); 
+					break;
+				case '%':
+					write(1, &format[i], 1);
+					count++;
+					break;
+				default:
+					count += def_prnt(format[i]);
+					i++;
+					break;
 			}
-			i++;
 		}
-		va_end(args);
-		return (count);
+		else
+		{
+			write(1, &format[i], 1);
+			count++;
+		}
 	}
-	else
 	va_end(args);
-	return (-1);
+	return (count);
 }
