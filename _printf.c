fef8e4a0979b7864a0ea6129a0e246b7dedd2d4a
prodjohnper <jonathanperez9743@gmail.com>
2023-11-21 10:59:24 -0400

    Update
    Refactored _printf function

--- _printf.c
+++ _printf.c
@@ -8,39 +8,32 @@
  */
 int _printf(const char *format, ...)
 {
-	int count = 0, i = 0;
-	va_list args;
+        int count = 0;
+        va_list args;
 
-	va_start(args, format);
+        va_start(args, format);
 
-	if (format == NULL)
-		return (-1);
-	while (format[i] != '\0')
-	{
-		if (format[i] == '%')
-		{
-			i++;
-
-			switch (format[i])
-			{
-				case 'c': case 's': case 'i': case 'd':
-					count += (format[i] == 'c') ? char_prnt(va_arg(args, int)) :
-						(format[i] == 's') ? str_prnt(va_arg(args, char *)) :
-						int_prnt(va_arg(args, int));
-					break;
-				default:
-					def_prnt();
-					count++;
-					break;
+        while (*format) {
+                if (*format != '%') {
+                        count += char_prnt(*format);
+                } else {
+                        format++;
+                        if (*format == '\0') {
+                                str_prnt("(null)");
+                                count += _strlen("(null)");
+                        } else if (*format == 's') {
+                                 count += str_prnt (va_arg(args, char *));
 			}
-		}
-		else
-		{
-			write(1, &format[i], 1);
-			count++;
-		}
-		i++;
-	}
-	va_end(args);
-	return (count);
+				if (*format == 'c') {
+                                	count += char_prnt(va_arg(args, int));
+                        } else if (*format == 'd' || *format == 'i') {
+                                count += prnt_d (va_arg(args, int));
+                        } else if (*format == '%') {
+                                count += char_prnt('%');
+                        }
+                }
+                format++;
+        }
+        va_end(args);
+        return (count);
 }
