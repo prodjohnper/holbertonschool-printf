a88e5396fd36368c04b029ce9c97c6f91d465f9a
prodjohnper <jonathanperez9743@gmail.com>
2023-11-21 11:30:31 -0400

    Add
    Added prnt_d function

---
+++ prnt_d.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * prnt_d - Function that prints an integer.
+ *
+ * @num; Integer to print.
+ *
+ * Return: Length of integer printed.
+ */
+
+int prnt_d(int num)
+{
+	char buffer[20];
+	int i = 0, j;
+
+	if (num == 0)
+	{
+		write(1, "0", 1);
+	}
+
+	else if (num < 0)
+	{
+		write(1, "-", 1);
+		num = -num;
+	}
+
+	while (num != 0)
+	{
+		buffer[i++] = num % 10 + '0';
+		num/= 10;
+	}
+
+	for (j = i; j >= 0; j--)
+	{
+		write(1, &buffer[j], 1);
+	}
+
+	return (i);
+}
