91b79988e3ea0f1074f69833adcbaeac186c25e3
prodjohnper <jonathanperez9743@gmail.com>
2023-11-21 20:59:25 -0400

    Fix
    Fixed error with betty coding style

--- _printf.c
+++ _printf.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _printf - Function that produces
  * output according to a format.
@@ -6,34 +7,34 @@
  *
  * Return: number of characters printed (excluding null byte).
  */
+
 int _printf(const char *format, ...)
 {
-        int count = 0;
-        va_list args;
+	int count = 0;
+	va_list args;
 
-        va_start(args, format);
+	va_start(args, format);
 
-        while (*format) {
-                if (*format != '%') {
-                        count += prnt_c(*format);
-                } else {
-                        format++;
-                        if (*format == '\0') {
-                                prnt_str("(null)");
-                                count += _strlen("(null)");
-                        } else if (*format == 's') {
-                                 count += prnt_str (va_arg(args, char *));
+	while (*format) {
+		if (*format != '%') {
+			count += prnt_c(*format);
+		} else {
+			format++;
+			if (*format == '\0') {
+				prnt_str("(null)");
+				count += _strlen("(null)");
+			} else if (*format == 's') {
+				count += prnt_str(va_arg(args, char *));
+			} else if (*format == 'c') {
+				count += prnt_c(va_arg(args, int));
+			} else if (*format == 'd' || *format == 'i') {
+				count += prnt_d(va_arg(args, int));
+			} else if (*format == '%') {
+				count += prnt_c('%');
 			}
-				if (*format == 'c') {
-                                	count += prnt_c(va_arg(args, int));
-                        } else if (*format == 'd' || *format == 'i') {
-                                count += prnt_d (va_arg(args, int));
-                        } else if (*format == '%') {
-                                count += prnt_c('%');
-                        }
-                }
-                format++;
-        }
-        va_end(args);
-        return (count);
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
 }
